19fb64fac8e5d7ed55cb133a200c3b9cf1196e38
vents04 <vencidim04@gmail.com>
2022-04-14 19:45:21 +0300

    google route reviewed and updated

--- gymRats/api/routes/google.route.js
+++ gymRats/api/routes/google.route.js
@@ -1,41 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
 const axios = require('axios');
-const { googlePlacesSearchValidation } = require('../validation/hapi');
-const { GOOGLE_API_KEY, HTTP_STATUS_CODES } = require('../global');
-const { authenticate } = require('../middlewares/authenticate');
+
 const ResponseError = require('../errors/responseError');
+
+const { authenticate } = require('../middlewares/authenticate');
+
+const { GOOGLE_API_KEY, HTTP_STATUS_CODES, DEFAULT_ERROR_MESSAGE } = require('../global');
 
 router.get('/search-places', authenticate, async (req, res, next) => {
     try {
-        axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.query}&key=${GOOGLE_API_KEY}&language=en&types=(cities)`)
-            .then((response) => {
-                res.status(HTTP_STATUS_CODES.OK).send({
-                    results: response.data.predictions
-                })
-            })
-            .catch((error) => {
-                throw new Error(error);
-            })
+        let response = await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.query}&key=${GOOGLE_API_KEY}&language=en&types=(cities)`);
+        return res.status(HTTP_STATUS_CODES.OK).send({
+            results: response.data.predictions
+        })
     } catch (e) {
-        return next(new ResponseError(e.message || "Internal server error", e.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+        return next(new ResponseError(e.message || DEFAULT_ERROR_MESSAGE, e.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
     }
 })
 
 router.get('/place/:id', authenticate, async (req, res, next) => {
     try {
-        axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${req.params.id}&key=${GOOGLE_API_KEY}`)
-            .then((response) => {
-                res.status(HTTP_STATUS_CODES.OK).send({
-                    result: response.data.result
-                })
-            })
-            .catch((error) => {
-                throw new Error(error);
-            })
+        let response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${req.params.id}&key=${GOOGLE_API_KEY}`);
+        return res.status(HTTP_STATUS_CODES.OK).send({
+            result: response.data.result
+        })
     } catch (e) {
-        return next(new ResponseError(e.message || "Internal server error", e.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+        return next(new ResponseError(e.message || DEFAULT_ERROR_MESSAGE, e.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
     }
 })
 
