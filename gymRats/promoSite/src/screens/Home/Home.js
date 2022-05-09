5a33c05beb673f2c3fc7fcadf31b9d8babf74a00
sv4t <73824869+sv4t@users.noreply.github.com>
2022-05-09 18:38:53 +0300

    promo site done

---
+++ gymRats/promoSite/src/screens/Home/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import "./Home.css"
+import logo from "../../../assets/images/icon.png"
+import google from "../../../assets/images/badges/google.png"
+import apple from "../../../assets/images/badges/apple.svg"
+import mockup from "../../../assets/images/mockup.png"
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="page-container">
+          <div className="logo-container">
+              <img className="logo-container-logo" src={logo} alt="logo"/>
+              <p className="logo-container-text">GYM RATS</p>
+          </div>
+          <div className="home-container">
+                <div className="incentive-container">
+                    <p className="incentive-container-text">The one and only <br/> app gym rats need</p>
+                    <div className="badges-container">
+                        <img className="google-badge" src={google} alt="google"/>
+                        <img className="apple-badge" src={apple} alt="apple"/>
+                    </div>
+                </div>
+                <div className="mockup-container">
+                    <img className="home-container-mockup" src={mockup} alt="mockup"/>
+                </div>
+          </div>
+      </div>
+    )
+  }
+}
---
+++ gymRats/promoSite/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|svg)$/i,
+                use: ['file-loader'],
+            },
+        ]
+    }
+};
