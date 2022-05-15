4b8497725716335a97e2ee83a951fe73f226e691
vents04 <vencidim04@gmail.com>
2022-05-15 23:30:34 +0300

    wieght tracker progress

--- gymRats/react-native/app/screens/Progress/Progress.js
+++ gymRats/react-native/app/screens/Progress/Progress.js
@@ -4,10 +4,14 @@ import { Dimensions, Image, ScrollView, Text, TextInput, TouchableOpacity, View 
 import ApiRequests from '../../classes/ApiRequests';
 
 import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
-import { HTTP_STATUS_CODES } from '../../../global';
+import { HTTP_STATUS_CODES, PROGRESS_NOTATION } from '../../../global';
 
 import globalStyles from '../../../assets/styles/global.styles';
+import { cardColors } from '../../../assets/styles/cardColors';
 import styles from './Progress.styles';
 import LogoBar from '../../components/LogoBar/LogoBar';
 
@@ -39,6 +43,7 @@ export default class Progress extends Component {
     getProgress = () => {
         ApiRequests.get(`progress/page`, {}, true).then((response) => {
             console.log(response)
+            this.setState({ progress: response.data });
         }).catch((error) => {
             if (error.response) {
                 if (error.response.status != HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR) {
@@ -59,6 +64,49 @@ export default class Progress extends Component {
             <View style={globalStyles.safeAreaView}>
                 <View style={globalStyles.pageContainer}>
                     <LogoBar />
+                    {
+                        this.state.progress
+                            ? <>
+                                {
+                                    this.state.progress.weightTrackerProgress
+                                        ? <View style={styles.progressCardContainer}>
+                                            <View style={styles.progressCardHeaderContainer}>
+                                                <FontAwesome5 name="weight" size={20} color={cardColors.weightTracker} />
+                                                <Text style={styles.progressCardHeader}>Weight tracker</Text>
+                                            </View>
+                                            <TouchableOpacity style={{ ...styles.progressFlagContainer, backgroundColor: cardColors.weightTracker }} onPress={() => { }}>
+                                                <Text style={styles.progressFlag}>
+                                                    {
+                                                        this.state.progress.weightTrackerProgress == PROGRESS_NOTATION.INSUFFICIENT_WEIGHT_LOSS
+                                                            || this.state.progress.weightTrackerProgress == PROGRESS_NOTATION.INSUFFICIENT_WEIGHT_GAIN
+                                                            ? "No change"
+                                                            : this.state.progress.weightTrackerProgress == PROGRESS_NOTATION.SUFFICIENT_WEIGHT_LOSS
+                                                                ? "Efficient weight loss"
+                                                                : this.state.progress.weightTrackerProgress == PROGRESS_NOTATION.SUFFICIENT_WEIGHT_GAIN
+                                                                    ? "Efficient weight gain"
+                                                                    : this.state.progress.weightTrackerProgress == PROGRESS_NOTATION.RAPID_WEIGHT_LOSS
+                                                                        ? "Too rapid weight loss"
+                                                                        : this.state.progress.weightTrackerProgress == PROGRESS_NOTATION.RAPID_WEIGHT_GAIN
+                                                                            ? "Too rapid weight gain"
+                                                                            : null
+
+                                                    }
+                                                </Text>
+                                                <Entypo name="info-with-circle" size={18} color="white" />
+                                            </TouchableOpacity>
+                                            <View style={styles.progressCardTips}>
+                                                <Text style={styles.progressCardTipsTitle}>Tips to improve</Text>
+                                                <View style={styles.progressCardTipContainer}>
+                                                    <AntDesign style={styles.progressCardTipIcon} name="checkcircle" size={16} color={cardColors.weightTracker} />
+                                                    <Text style={styles.progressCardTip}>Fasting 16 hours a day may help you lose weight quicker</Text>
+                                                </View>
+                                            </View>
+                                        </View>
+                                        : null
+                                }
+                            </>
+                            : null
+                    }
                 </View>
             </View>
         )
--- gymRats/react-native/app/screens/Progress/Progress.styles.js
+++ gymRats/react-native/app/screens/Progress/Progress.styles.js
@@ -1,5 +1,58 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-
+    progressCardContainer: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 4,
+        padding: 16
+    },
+    progressCardHeaderContainer: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 16
+    },
+    progressCardHeader: {
+        fontFamily: "MainBold",
+        fontSize: 12.8,
+        marginLeft: 8,
+        textTransform: "uppercase",
+        color: "#aaa"
+    },
+    progressFlagContainer: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderRadius: 4,
+        padding: 8,
+    },
+    progressFlag: {
+        fontFamily: "MainBold",
+        fontSize: 10,
+        marginRight: 8,
+        textTransform: "uppercase",
+        color: "white"
+    },
+    progressCardTips: {
+        marginTop: 16,
+    },
+    progressCardTipsTitle: {
+        fontFamily: "MainBlack",
+        textTransform: "uppercase",
+        fontSize: 14,
+        color: "#262626"
+    },
+    progressCardTipContainer: {
+        display: "flex",
+        flexDirection: "row",
+        marginVertical: 8,
+        marginRight: 16
+    },
+    progressCardTip: {
+        fontFamily: "MainRegular",
+        fontSize: 12,
+        marginLeft: 8
+    }
 })
