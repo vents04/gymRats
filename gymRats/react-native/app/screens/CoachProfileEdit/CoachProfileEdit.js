1ccc21d5c574b938aa536e5657d868808459bc85
vents04 <vencidim04@gmail.com>
2022-06-19 19:19:52 +0300

    url share coach updated

--- gymRats/react-native/app/screens/CoachProfileEdit/CoachProfileEdit.js
+++ gymRats/react-native/app/screens/CoachProfileEdit/CoachProfileEdit.js
@@ -59,7 +59,7 @@ export default class CoachPage extends Component {
 
     shareProfileLink = async () => {
         try {
-            const url = `gymrats://coach-profile/${this.state.coach._id}`
+            const url = `https://gymrats.uploy.app/coach-profile/${this.state.coach._id}`
             await Share.share({
                 message: `Be coached by ${this.state.coach.firstName}!\n${url}`,
             });
