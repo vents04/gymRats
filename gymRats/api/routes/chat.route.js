36c480a0a0cd245e5a75f9a02742011241dd972b
sv4t <73824869+sv4t@users.noreply.github.com>
2022-06-19 17:44:38 +0300

    get chat but limited to 25 messages

--- gymRats/api/routes/chat.route.js
+++ gymRats/api/routes/chat.route.js
@@ -93,6 +93,49 @@ router.get('/:id', authenticate, async (req, res, next) => {
     }
 });
 
+router.get('/:id/:lastMessageId', authenticate, async (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) return next(new ResponseError("Invalid chat id", HTTP_STATUS_CODES.BAD_REQUEST, 5));
+    if (!mongoose.Types.ObjectId.isValid(req.params.lastMessageId)) return next(new ResponseError("Invalid message id", HTTP_STATUS_CODES.BAD_REQUEST, 5));
+
+    try {
+        const chat = await DbService.getById(COLLECTIONS.CHATS, req.params.id);
+        if (!chat) return next(new ResponseError("Chat not found", HTTP_STATUS_CODES.NOT_FOUND, 22));
+
+        const personalTrainer = await DbService.getOne(COLLECTIONS.PERSONAL_TRAINERS, { userId: mongoose.Types.ObjectId(req.user._id) })
+        if (personalTrainer && personalTrainer._id.toString() != chat.personalTrainerId.toString())
+        return next(new ResponseError("You cannot access chats in which you are not a participant!", HTTP_STATUS_CODES.FORBIDDEN, 23));
+
+        if(!personalTrainer && chat.clientId.toString() != req.user._id.toString())
+        return next(new ResponseError("You cannot access chats in which you are not a participant!", HTTP_STATUS_CODES.FORBIDDEN, 23));
+
+        let oppositeUser = null;
+        if (personalTrainer && (chat.personalTrainerId.toString() == personalTrainer._id.toString())) oppositeUser = await DbService.getOne(COLLECTIONS.USERS, { _id: mongoose.Types.ObjectId(chat.clientId) });
+        if (chat.clientId.toString() == req.user._id.toString()) {
+            const coach = await DbService.getById(COLLECTIONS.PERSONAL_TRAINERS, chat.personalTrainerId);
+            if (!coach) return next(new ResponseError("Coach was not found", HTTP_STATUS_CODES.NOT_FOUND, 17));
+            oppositeUser = await DbService.getById(COLLECTIONS.USERS, coach.userId);
+        }
+        if (!oppositeUser) return next(new ResponseError("Opposite user not found", HTTP_STATUS_CODES.NOT_FOUND, 24));
+
+        let messages;
+        if(!req.params.lastMessageId){
+            messages = await DbService.getManyWithLimit(COLLECTIONS.MESSAGES, { chatId: mongoose.Types.ObjectId(req.params.id) }, 25);
+        }else{
+            const lastMessage = await DbService.getOne(COLLECTIONS.MESSAGES, { _id: mongoose.Types.ObjectId(req.params.lastMessageId) });
+            if(!lastMessage) return next(new ResponseError("Message not found", HTTP_STATUS_CODES.NOT_FOUND, 59));
+            messages = await DbService.getManyWithLimit(COLLECTIONS.MESSAGES, { chatId: mongoose.Types.ObjectId(req.params.id), createdDt: { $lt: lastMessage.createdDt } }, 25);
+        }
+
+        Object.assign(chat, { user: req.user }, { oppositeUser: oppositeUser }, { messages: messages });
+
+        return res.status(HTTP_STATUS_CODES.OK).send({
+            chat
+        })
+    } catch (err) {
+        return next(new ResponseError(err.message || DEFAULT_ERROR_MESSAGE, err.status || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR));
+    }
+});
+
 router.put('/:id/seen', authenticate, async function (req, res, next) {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) return next(new ResponseError("Invalid chat id", HTTP_STATUS_CODES.BAD_REQUEST, 5));
 
