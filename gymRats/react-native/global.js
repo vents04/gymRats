e8050c0fffd8f711e9738b07b987e8b6717ff29b
Georgi Zarev <georgi.zarev0304@gmail.com>
2022-10-25 11:32:17 +0300

    trid to fix the react native bugs

--- gymRats/react-native/global.js
+++ gymRats/react-native/global.js
@@ -1,6 +1,6 @@
 const AUTHENTICATION_TOKEN_KEY = "x-auth-token";
 
-const ROOT_URL_API = "https://api.gymrats.uploy.app";
+const ROOT_URL_API = "https://192.168.31.94:4057";
 
 const HTTP_STATUS_CODES = {
     OK: 200,
