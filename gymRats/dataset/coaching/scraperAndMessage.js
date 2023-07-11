30e0cc8cc4d384fd345bec50a0dde4509c8c2c17
vents04 <vencidim04@gmail.com>
2023-07-11 04:39:33 +0300

    stripe connect fields added to teacher's account

---
+++ gymRats/dataset/coaching/scraperAndMessage.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async function () {
+    let browser;
+    try {
+        console.log("Opening the browser......");
+        browser = await puppeteer.launch({
+            headless: false,
+            args: ["--disable-setuid-sandbox"],
+            'ignoreHTTPSErrors': true
+        });
+    } catch (err) {
+        console.log("Could not create a browser instance => : ", err);
+    }
+})();
